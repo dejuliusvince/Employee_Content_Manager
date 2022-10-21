d0e1a46577c1cb95c5a9bd41b890820dc65c946f
vincedejulius <dejuliusvince@gmail.com>
2022-10-21 19:35:12 -0400

    complete tables for employee, role, and department in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,7 @@
+/*Need to write sql schema tables according to directions on the readMe
+-Each table needs primary key id to create relationships 
+*/
+
 DROP DATABASE IF EXISTS  employee_db;
 
 CREATE DATABASE employee_db;
@@ -7,11 +11,32 @@ USE employee_db;
 CREATE TABLE department(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30)
-;)
+);
 
 CREATE TABLE role(
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30),
+  /*DECIMAL needs 2 arguments to specify prescision*/
   salary DECIMAL(10,2)
-  department_id INT NOT NULL 
+  department_id INT,
+  /*parent department sharing id with child table role*/
+  FOREIGN KEY(department_id) REFERENCES department (id)
+  ON DELETE CASCADE 
+
 );
+
+CREATE TABLE employee(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY(role_id) REFERENCES role (id)
+  ON DELETE CASCADE,
+  /*need to self-reference because manager_id uses the same table as id to set the relationship,
+  use SET NULL instead of CASCADE to delete a manager without deleting all of their employees
+  */
+  FOREIGN KEY(manager_id) REFERENCES employee (id) ON DELETE SET NULL,
+
+);
+
