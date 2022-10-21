7352bcd622b7e2208572ced41acd0bbb2d18ae7e
vincedejulius <dejuliusvince@gmail.com>
2022-10-21 19:18:04 -0400

    begin writing schema tables

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS  employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE department(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30)
+;)
+
+CREATE TABLE role(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL(10,2)
+  department_id INT NOT NULL 
+);
